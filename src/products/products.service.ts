1f5dca1bfb7dfb69d85562e7bbe5324cd330e237
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-08-04 01:24:14 -0500

    manejo de errores

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,6 +7,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
@@ -20,8 +22,7 @@ export class ProductsService {
 
       return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Ayuda');
+      this.handleDBExceptions(error);
     }
   }
 
@@ -39,5 +40,14 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${ id } product`;
+  }
+
+  private handleDBExceptions(error: any) {
+    // console.error({ error });
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error -  Check Server logs');
   }
 }
