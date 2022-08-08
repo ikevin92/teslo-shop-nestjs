116ee9559bb9cfe26c612804cb762dd203e5966e
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-08-07 21:55:24 -0500

    servir archivo de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,20 +1,31 @@
-import { BadGatewayException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadGatewayException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { fileFilter, fileNamer } from './helpers';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path)
+  }
+
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
     fileFilter: fileFilter, // valida el tipo de archivo
     //limits: { fileSize: 1000 }, // tamaño del archivo
     storage: diskStorage({
-      destination: './static/uploads'
+      destination: './static/uploads',
+      filename: fileNamer
     })
   }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
@@ -23,8 +34,10 @@ export class FilesController {
       throw new BadGatewayException('Make sure that the file is an image');
     }
 
+    const secureUrl = `${ file.filename }`;
+
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
