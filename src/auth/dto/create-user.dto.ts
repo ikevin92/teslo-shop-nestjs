647668091b3fc3a36e1e01da4a216598b2a71ca1
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-10-04 09:26:02 -0500

    documentacion auth dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,11 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, MaxLength, Matches, IsEmail } from 'class-validator';
 
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User email',
+    nullable: false,
+    minLength: 1,
+    maxLength: 255,
+    format: 'email',
+    example: 'usuario@mail.com'
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    nullable: false,
+    minLength: 6,
+    maxLength: 50,
+    example: '12345678'
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
@@ -15,6 +31,13 @@ export class CreateUserDto {
   })
   password: string;
 
+  @ApiProperty({
+    description: 'User name',
+    nullable: false,
+    minLength: 1,
+    maxLength: 6,
+    example: 'John Doe'
+  })
   @IsString()
   @MinLength(6)
   fullName: string;
--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, MaxLength, Matches, IsEmail } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'User email',
+    nullable: false,
+    minLength: 1,
+    maxLength: 255,
+    format: 'email',
+    example: 'usuario@mail.com'
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    nullable: false,
+    minLength: 6,
+    maxLength: 50,
+    example: '12345678'
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
